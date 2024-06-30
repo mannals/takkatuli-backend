29f4ded278d91dc0df8d942de728ff4f25a3bc89
mannals <annamalassu@gmail.com>
2024-06-30 12:45:35 +0300

    pm2 toimimaan

--- index.js
+++ index.js
@@ -1,32 +1,60 @@
 const express = require('express');
-const concurrently = require('concurrently');
 const path = require('path');
+const pm2 = require('pm2');
 
 const app = express();
 const port = 3003;
 
-async function startServers() {
-    const commands = [
-        { command: 'npm run start --prefix ./auth-server', name: 'auth-server' },
-        { command: 'npm run start --prefix ./media-api', name: 'media-api' },
-        { command: 'npm run start --prefix ./upload-server', name: 'upload-server' },
-    ];
+const startServers = () => {
+    pm2.connect(err => {
+        if (err) {
+            console.error(err);
+            process.exit(2);
+        }
 
-    try {
-        await concurrently(commands);
-        console.log('All servers started successfully');
+        const servers = [
+            {script: './auth-server/dist/auth-server/src/index.js', name: 'auth-server', port: 3001},
+            {script: './media-api/dist/media-api/src/index.js', name: 'media-api', port: 3000},
+            {script: './upload-server/dist/upload-server/src/index.js', name: 'upload-server', port: 3002}
+        ];
 
-    } catch (error) {
-        console.error('Failed to start one or more servers', error);
-    }
-}
+        servers.forEach(server => {
+            const options = {
+                ...server,
+                env: {
+                    ...process.env,
+                    PORT: server.port
+                }
+            };
+
+            pm2.start(options, (err, apps) => {
+                if (err) {
+                    console.error(`Failed to start ${server.name}`, err);
+                    process.exit(2);
+                }
+                console.log(`${server.name} started successfully on port http://localhost:${server.port}`);
+            });
+        });
+
+        pm2.launchBus((err, bus) => {
+            bus.on('process:msg', (packet) => {
+              console.log('[PM2] Log: ', packet);
+            });
+        });
+    });
+};
 
 startServers();
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
-  });
+});
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
-  });
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack); // Log the error stack to the console
+    res.status(500).send('Something broke!'); // Respond with a generic error message
+});
